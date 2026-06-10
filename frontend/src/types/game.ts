1ae1d088ab86f821082902ecf6bf36446603b428
Soraya Fernandez Ramirez <sofernan@c2r14s5.42madrid.com>
2026-06-10 17:21:07 +0200

    eliminar archivos y código innecesario

--- frontend/src/types/game.ts
+++ frontend/src/types/game.ts
@@ -1,37 +1,3 @@
-/*export type PlayerColor =
-  | "pink"
-  | "purple"
-  | "green"
-  | "blue";
-
-export interface Piece {
-  id: number;
-  position: number;
-  isHome: boolean;
-  isFinished: boolean;
-}
-
-export interface Player {
-  id: number;
-  username: string;
-  color: PlayerColor;
-  pieces: Piece[];
-}
-
-export interface Game {
-  id: string;
-
-  players: Player[];
-
-  currentTurn: number;
-
-  diceValue: number | null;
-
-  status:
-    | "waiting"
-    | "playing"
-    | "finished";
-}*/
 
 export type PieceState =
   | "base"

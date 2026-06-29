e22216df0e946452a99f176af2e3f51c7d54376a
Victor Diez Cuesta <vdiez-cu@c1r15s6.42madrid.com>
2026-06-29 17:18:02 +0200

    implementacion chat lobby

--- backend/sockets/index.js
+++ backend/sockets/index.js
@@ -1,11 +1,16 @@
 const { Server } = require('socket.io');
 const authSocket = require('./authSocket');
 const registerGameSocket = require('./gameSocket');
-const { setIO } = require('../socket');
-const {addUserSocket, removeUserSocket, isUserOnline} = require('./presence');
+const registerLobbySocket = require('./lobbySocket');
 
-function initSockets(httpServer)
-{
+const { setIO } = require('../socket');
+const {
+	addUserSocket,
+	removeUserSocket,
+	isUserOnline
+} = require('./presence');
+
+function initSockets(httpServer) {
 	const io = new Server(httpServer, {
 		cors: {
 			origin: true,
@@ -15,22 +20,30 @@ function initSockets(httpServer)
 	});
 
 	setIO(io);
+
 	io.use(authSocket);
+
 	io.on('connection', (socket) => {
 		const userId = socket.user.id;
+
 		addUserSocket(userId, socket.id);
-		io.emit('presence:update', {userId, online: true});
+
+		io.emit('presence:update', {
+			userId,
+			online: true
+		});
+
 		registerGameSocket(io, socket);
+		registerLobbySocket(io, socket);
+
 		socket.on('disconnect', () => {
 			removeUserSocket(userId, socket.id);
-			if (!isUserOnline(userId))
-			{
-				io.emit('presence:update',
-					{
-						userId,
-						online: false
-					}
-				);
+
+			if (!isUserOnline(userId)) {
+				io.emit('presence:update', {
+					userId,
+					online: false
+				});
 			}
 		});
 	});
